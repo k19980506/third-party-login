fa93657e56c877e83ffc9eeaa5497cf2effc1ed1
aShangxuanYang <shang-xuan.yang@access-company.com>
2022-05-21 21:49:45 +0800

    Add routes

---
+++ app/routes/user.routes.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+	const users = require("../controllers/user.controller");
+	var router = require("express").Router();
+
+	router.post("/", users.create);
+	router.get("/", users.retrieveList);
+	router.get("/:id", users.retrieve);
+	router.put("/:id", users.update);
+	router.delete("/:id", users.delete);
+};
